56d66c0203be10fbd76e024eeeafd6a91344cb8d
smoyamedina <smoyamedina@outlook.com>
2023-02-25 00:20:09 -0500

    Fetch array of documents for given _type

---
+++ src/__fetch/projects.ts
@@ -0,0 +1,10 @@
+fetch(
+	"https://vzl61mfy.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == 'project']"
+)
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+		return response.json();
+	})
+	.then((data) => console.log(data.result));
