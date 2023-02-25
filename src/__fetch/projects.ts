3d1d90821195b2b4babb5fa7c3df6f8252035b1d
smoyamedina <smoyamedina@outlook.com>
2023-02-25 14:55:08 -0500

    Wait for promise to fulfill to export

--- src/__fetch/projects.ts
+++ src/__fetch/projects.ts
@@ -1,10 +1,13 @@
-fetch(
-	"https://vzl61mfy.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == 'project']"
-)
-	.then((response) => {
-		if (!response.ok) {
-			throw new Error(response.statusText);
-		}
-		return response.json();
-	})
-	.then((data) => data.result);
+export = async () => {
+	let response = await fetch(
+		"https://vzl61mfy.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == 'project']"
+	);
+
+	if (!response.ok) {
+		throw new Error(response.statusText);
+	}
+
+	let json = await response.json();
+
+	return json.result;
+};
