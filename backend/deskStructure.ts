1a85e27d9e02a7f8ae41b71f4337a363b49b0520
smoyamedina <smoyamedina@outlook.com>
2023-02-27 16:58:40 -0500

    Allow for adding subsequent single types

--- backend/deskStructure.ts
+++ backend/deskStructure.ts
@@ -1,11 +1,21 @@
 // deskStructure.ts
 // *** Reference used: https://www.sanity.io/docs/structure-builder-reference#1e6a04652e80
 // todo--make reusable
-export const myStructure = (S: any) =>
-  S.list()
+export const myStructure = (S: any) => {
+  let singleTypes = [
+    {name: 'selectWork', title: 'My Work'},
+    {name: 'about', title: 'About Me'},
+  ]
+
+  return S.list()
     .title('Content Types')
     .items([
-      S.documentListItem().id('selectWork').schemaType('selectWork').title('My Work'),
+      ...singleTypes.map((item) => {
+        return S.documentListItem().id(item.name).schemaType(item.name).title(item.title)
+      }),
       S.divider(),
-      ...S.documentTypeListItems().filter((item: any) => item.getId() !== 'selectWork'),
+      ...S.documentTypeListItems().filter(
+        (item: any) => !singleTypes.map((item) => item.name).includes(item.getId())
+      ),
     ])
+}
