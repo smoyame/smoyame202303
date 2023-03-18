2e7d4e41d2f19c8ce35ff1c33b75852725c63116
smoyamedina <smoyamedina@outlook.com>
2023-03-18 01:05:14 -0400

    prep 404

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,7 @@ module.exports = function (eleventyConfig) {
 	});
 
 	eleventyConfig.addPassthroughCopy("./src/_assets");
+	eleventyConfig.addPassthroughCopy("./src/.htaccess");
 
 	return {
 		dir: {
