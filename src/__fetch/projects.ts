b2604e07c9759592ef7b053e4f70935f95c655b6
smoyamedina <smoyamedina@outlook.com>
2023-03-25 20:52:36 -0400

    Flatten data fetched from content lake

--- src/__fetch/projects.ts
+++ src/__fetch/projects.ts
@@ -1,6 +1,6 @@
 export = async () => {
 	let response = await fetch(
-		"https://vzl61mfy.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'selectWork'%5D%7B%0A%20%20list%5B%5D-%3E%0A%7D.list%5B%5D%7B%0A%20%20...%2C%0A%20%20cover%7B%0A%20%20%20%20...%2C%0A%20%20%22URL%22%3A%20asset-%3Eurl%7D%2C%0A%20%20content%5B%5D%7B%0A%20%20%20%20...%2C%20%0A%20%20%20%20item%7B...%2C%20%22URL%22%3A%20asset-%3Eurl%7D%0A%20%20%7D%0A%7D"
+		"https://vzl61mfy.api.sanity.io/v2021-10-21/data/query/production?query=%0A*%5B_type%20%3D%3D%20'selectWork'%5D%7B%0A%20%20...%2Clist%0A%7D.list%5B%5D%7Bitem-%3E%7B%20...%2C%22grid%22%3A%20%5E.grid%7D%7D.item%7B%0A%20%20...%2C%0A%20%20cover%7B...%2C%22URL%22%3A%20asset-%3Eurl%7D%2C%0A%20%20%22content%22%3A%20content%5B%5D.item%7B%0A%20%20%20%20...%2C%20%0A%20%20%20%20%22URL%22%3A%20asset-%3Eurl%0A%20%20%7D%2C%0A%20%20info%7B%0A%20%20%20%20prompt%2C%0A%20%20%20%20%22grafs%22%3A%20desc%5B%5D.children%5B%5D.text%0A%20%20%7D%2C%0A%7D"
 	);
 
 	if (!response.ok) {
--- src/_data/projects.js
+++ src/_data/projects.js
@@ -39,7 +39,7 @@ module.exports = function () { return __awaiter(void 0, void 0, void 0, function
     var response, json;
     return __generator(this, function (_a) {
         switch (_a.label) {
-            case 0: return [4 /*yield*/, fetch("https://vzl61mfy.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'selectWork'%5D%7B%0A%20%20list%5B%5D-%3E%0A%7D.list%5B%5D%7B%0A%20%20...%2C%0A%20%20cover%7B%0A%20%20%20%20...%2C%0A%20%20%22URL%22%3A%20asset-%3Eurl%7D%2C%0A%20%20content%5B%5D%7B%0A%20%20%20%20...%2C%20%0A%20%20%20%20item%7B...%2C%20%22URL%22%3A%20asset-%3Eurl%7D%0A%20%20%7D%0A%7D")];
+            case 0: return [4 /*yield*/, fetch("https://vzl61mfy.api.sanity.io/v2021-10-21/data/query/production?query=%0A*%5B_type%20%3D%3D%20'selectWork'%5D%7B%0A%20%20...%2Clist%0A%7D.list%5B%5D%7Bitem-%3E%7B%20...%2C%22grid%22%3A%20%5E.grid%7D%7D.item%7B%0A%20%20...%2C%0A%20%20cover%7B...%2C%22URL%22%3A%20asset-%3Eurl%7D%2C%0A%20%20%22content%22%3A%20content%5B%5D.item%7B%0A%20%20%20%20...%2C%20%0A%20%20%20%20%22URL%22%3A%20asset-%3Eurl%0A%20%20%7D%2C%0A%20%20info%7B%0A%20%20%20%20prompt%2C%0A%20%20%20%20%22grafs%22%3A%20desc%5B%5D.children%5B%5D.text%0A%20%20%7D%2C%0A%7D")];
             case 1:
                 response = _a.sent();
                 if (!response.ok) {
